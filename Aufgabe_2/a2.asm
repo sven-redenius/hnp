8cb450728df24aca1c09c45df0eb43ea4ae8b4c5
Sven Redenius <sv6065@hs-el.de>
2024-10-10 17:36:45 +0200

    Refactor code to use register indirect addressing for displaying "Hallo" on the 7-segment display

--- Aufgabe_2/a2.asm
+++ Aufgabe_2/a2.asm
@@ -13,21 +13,21 @@ DISPLAY_7 equ 9eh ; Adresse der 7-Segmentanzeige (ganz links)
 ;gibt den Text "Hallo" auf der 7-Segmentanzeige aus
 START: 
     mov al, 0x48 ; H
-    mov [DISPLAY_0], al
+    mov DISPLAY_0, [al]
     mov al, 0x61 ; a
-    mov [DISPLAY_1], al
+    mov DISPLAY_1, [al]
     mov al, 0x6c ; l
-    mov [DISPLAY_2], al
+    mov DISPLAY_2, [al] 
     mov al, 0x6c ; l
-    mov [DISPLAY_3], al
+    mov DISPLAY_3, [al]
     mov al, 0x6f ; o
-    mov [DISPLAY_4], al
+    mov DISPLAY_4, [al]
     mov al, 0x20 ; Leerzeichen
-    mov [DISPLAY_5], al
+    mov DISPLAY_5, [al] 
     mov al, 0x20 ; Leerzeichen
-    mov [DISPLAY_6], al
+    mov DISPLAY_6, [al]
     mov al, 0x20 ; Leerzeichen
-    mov [DISPLAY_7], al
-    jmp START
+    mov DISPLAY_7, [al]
+    
     
 
